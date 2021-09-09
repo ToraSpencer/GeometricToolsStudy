f5c60572b173c843b134b5edecdab84c04bcb0dd
wuhan <340473925@qq.com>
2021-09-09 19:24:30 +0800

    .

--- GTE/Samples/Geometrics/ConformalMapping/ConformalMappingWindow3.cpp
+++ GTE/Samples/Geometrics/ConformalMapping/ConformalMappingWindow3.cpp
@@ -1,9 +1,3 @@
-// David Eberly, Geometric Tools, Redmond WA 98052
-// Copyright (c) 1998-2021
-// Distributed under the Boost Software License, Version 1.0.
-// https://www.boost.org/LICENSE_1_0.txt
-// https://www.geometrictools.com/License/Boost/LICENSE_1_0.txt
-// Version: 4.0.2019.08.13
 
 #include "ConformalMappingWindow3.h"
 #include <Graphics/VertexColorEffect.h>
